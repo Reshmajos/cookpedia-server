059e0382e38118f8ea27c262ec5c2838c35c6038
Reshmajos <reshmajose1808@gmail.com>
2026-01-21 13:01:59 +0530

    registration logic completed

--- routes/routing.js
+++ routes/routing.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const recipeController = require('../controller/recipeController')
+const userController = require('../controller/userController')
 
 const router = new express.Router()
 
 // get all recipes
 router.get('/recipes',recipeController.getAllRecipesController)
+// register
+router.post('/register',userController.registerController)
 
 module.exports = router
